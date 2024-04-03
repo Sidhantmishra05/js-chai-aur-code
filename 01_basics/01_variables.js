f35ce54e7aef9989e015855f40c6857b43c1ba7e
Sidhantmishra05 <113235354+Sidhantmishra05@users.noreply.github.com>
2024-04-03 16:11:28 +0000

    Variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const account_Id=1429
+let account_Email="sidhant@gmail.com"
+var account_Password="1234"
+account_City="Siliguri"
+let account_Name;
+
+account_Email="sid@gmail.com"
+account_Password="798"
+console.table([account_Id,account_Email,account_Password,account_City,account_Name]);
