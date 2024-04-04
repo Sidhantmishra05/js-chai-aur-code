51b01708982b7fc7c08276833e013455366a0a8e
Sidhantmishra05 <113235354+Sidhantmishra05@users.noreply.github.com>
2024-04-04 15:12:12 +0000

    02_datatype done

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) //will not run this way since we are using nodejs not browser
+
+//DataType
+// number = 18,437,736,874,454,810,627 (that is, 2**64 - 2**53 + 3) values
+//BigInt 
+//boolean= true/false
+//string= " " (2**53 -1) values
+//null = standalone value
+//undefine 
+//symbol = unique
+
+//object
+
+let name= "Sidhant"
+let age=21
+
+console.log(typeof null)//null is a object Type
+console.log(typeof undefined);// undefined type
+
