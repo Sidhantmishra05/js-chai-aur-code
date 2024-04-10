5636df7a80c1608af91b69dae881a9bfc3a8d1fb
Sidhantmishra05 <113235354+Sidhantmishra05@users.noreply.github.com>
2024-04-10 16:02:17 +0000

    String Done

---
+++ 01_basics/05_string.js
@@ -0,0 +1,18 @@
+const name = "Sidhant"
+const Age= 21
+
+console.log(name+ Age)
+console.log(`My name is ${name} and age is ${Age}`) //(prefered way)
+
+const game_name= new String("Sidhant_05")
+
+console.log(game_name.length);
+console.log(game_name[0]);
+console.log(game_name.toUpperCase());
+console.log(game_name.__proto__);
+console.log(game_name.charAt(3));
+console.log(game_name.indexOf('a'));
+console.log(game_name.blink())
+console.log(game_name.fontcolor(red))
+
+
