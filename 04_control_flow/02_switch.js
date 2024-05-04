5a0e1c4630a0c91192ce3dc7d7edab54d5100282
Sidhant Mishra <113235354+Sidhantmishra05@users.noreply.github.com>
2024-05-04 12:24:31 +0000

    02_switch done

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,20 @@
+const month = "march";
+
+switch (month) {
+    case "january":
+        console.log("01");
+        break;
+    case "febuary":
+        console.log("02");
+        break;
+    case "march":
+        console.log("03");
+        break;
+    case "April":
+        console.log("04");
+        break;
+    
+    default:
+        console.log("Default Case Matched");
+        break;
+}
