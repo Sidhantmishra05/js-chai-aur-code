47fa2553085d9579928aa99f717f699d54c0fbb5
Sidhant Mishra <113235354+Sidhantmishra05@users.noreply.github.com>
2024-06-24 07:58:53 +0000

    02_loops done

---
+++ 05_iterations/02_loops.js
@@ -0,0 +1,16 @@
+let index=0;
+while (index<10) {
+    console.log(`Value of inndex is ${index}`);
+    index=index+2
+}
+let myArray=['Sid','Sidhant','Anish']
+let arr=0;
+while (arr<myArray.length) {
+    console.log(`Name is ${myArray[arr]}`);
+    arr++;
+}
+let score=11;//1
+do {
+    console.log(score);
+    score++
+} while (score<10);
